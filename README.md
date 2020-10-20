75a1e0b45a807dc0f071cdac387c3e6faa7cdbb7
Andrei Alecu <andrei.alecu@vecttor.co>
2020-10-20 17:14:39 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# yarn-plugin-perworkspace-lockfile
+
+Creates a separate lockfile named `yarn.lock-workspace` in each workspace in a yarn 2+ project, containing only dependencies pertaining to that specific workspace.
+
+Usage:
+
+```
+yarn plugin import ...
+
+yarn install
+```
---
+++ packages/plugin/sources/index.ts
@@ -0,0 +1,109 @@
+import {
+  Plugin,
+  Hooks,
+  Project,
+  Configuration,
+  Cache,
+  Workspace,
+  Manifest,
+  StreamReport,
+  ThrowReport,
+} from "@yarnpkg/core";
+import { getPluginConfiguration } from "@yarnpkg/cli";
+
+import * as fs from "fs";
+import * as path from "path";
+import chalk from "chalk";
+
+const createLockfile = async (
+  configuration: Configuration,
+  { cwd }: Workspace
+) => {
+  const { project, workspace } = await Project.find(configuration, cwd);
+  const cache = await Cache.find(configuration);
+
+  let requiredWorkspaces: Set<Workspace> = new Set([workspace]);
+
+  // First we compute the dependency chain to see what workspaces are
+  // dependencies of the one we're trying to focus on.
+  //
+  // Note: remember that new elements can be added in a set even while
+  // iterating over it (because they're added at the end)
+
+  for (const workspace of requiredWorkspaces) {
+    for (const dependencyType of Manifest.hardDependencies) {
+      for (const descriptor of workspace.manifest
+        .getForScope(dependencyType)
+        .values()) {
+        const matchingWorkspace = project.tryWorkspaceByDescriptor(descriptor);
+
+        if (matchingWorkspace === null) continue;
+
+        //requiredWorkspaces.add(matchingWorkspace);
+      }
+    }
+  }
+
+  // remove any workspace that isn't a dependency, iterate in reverse so we can splice it
+  for (let i = project.workspaces.length - 1; i >= 0; i--) {
+    const currentWorkspace = project.workspaces[i];
+    if (!requiredWorkspaces.has(currentWorkspace)) {
+      project.workspaces.splice(i, 1);
+    }
+  }
+
+  await project.resolveEverything({
+    cache,
+    report: new ThrowReport(),
+  });
+
+  for (const w of project.workspaces) {
+    const pkg = Array.from(project.originalPackages.values()).find(
+      (p) => p.identHash === w.locator.identHash
+    );
+    if (pkg?.reference.startsWith("workspace:")) {
+      // ensure we replace the path in the lockfile from `workspace:packages/somepath` to `workspace:.`
+      if (w.cwd === cwd) {
+        pkg.reference = `workspace:.`;
+
+        Array.from(project.storedDescriptors.values()).find(
+          (v) => v.identHash === pkg.identHash
+        ).range = `workspace:.`;
+      }
+    }
+  }
+
+  return project.generateLockfile();
+};
+
+const plugin: Plugin<Hooks> = {
+  hooks: {
+    afterAllInstalled: async (project) => {
+      const configuration = await Configuration.find(
+        project.cwd,
+        getPluginConfiguration()
+      );
+
+      await StreamReport.start(
+        {
+          configuration,
+          stdout: process.stdout,
+          includeLogs: true,
+        },
+        async (report: StreamReport) => {
+          for (const workspace of project.workspaces) {
+            const lockPath = path.join(workspace.cwd, "yarn.lock-workspace");
+
+            fs.writeFileSync(
+              lockPath,
+              await createLockfile(configuration, workspace)
+            );
+            report.reportInfo(null, `${chalk.green(`✓`)} Wrote ${lockPath}`);
+          }
+        }
+      );
+    },
+  },
+};
+
+export default plugin;
